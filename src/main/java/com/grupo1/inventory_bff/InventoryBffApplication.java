41a77e618ff00559c6fbaac5bce883b80655abb5
Nathan <nathan@MacBook-Pro-de-Nathan.local>
2025-10-05 17:51:00 -0300

    Feat(main): BFF para azure functiond

---
+++ src/main/java/com/grupo1/inventory_bff/InventoryBffApplication.java
@@ -0,0 +1,13 @@
+package com.grupo1.inventory_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo1/inventory_bff/InventoryBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1.inventory_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
